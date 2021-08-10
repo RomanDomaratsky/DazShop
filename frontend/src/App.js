ac848ec61b234ee32cd0c6a8f39fa8f6441eca32
Domaratskiy Roman <roma.domaratsky@gmail.com>
2021-08-10 23:27:13 +0300

    Homepage: added navbar

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <div >
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to DazShop</h1>
+        </Container>
+      </main>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+function Footer() {
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className="text-center py-3">Copyricght &copy; DazShop</Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer
