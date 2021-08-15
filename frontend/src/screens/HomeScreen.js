423274f02799f3e93f55146b7eaabf6058e71b7e
Domaratskiy Roman <roma.domaratsky@gmail.com>
2021-08-15 23:29:50 +0300

    connected to backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import products from '../products'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import axios from 'axios'
+
 
 
 function HomeScreen() {
+    const [products, setProducts] = useState([])
+
+
+    useEffect(() => {
+
+        async function fetchProducts(){
+            const { data } = await axios.get("http://127.0.0.1:8000/api/products/")
+            setProducts(data)
+        }
+
+        fetchProducts()
+    }, [])
+
     return (
         <div>
             <h1>Latest Products</h1>
